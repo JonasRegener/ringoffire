4f90ca3fda5468b04ea65375c333b57bb0a7ba54
JonasRegener <regener.jonas@gmail.com>
2022-02-14 00:47:08 +0100

    bug fix

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  
+  
 
   constructor() { }
 
   ngOnInit(): void {
+  
   }
+
   takeCard() {
     this.pickCardAnimation = true;
 
