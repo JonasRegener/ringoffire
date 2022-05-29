a036d6608e730b7e2f4c1f3db370e812c62949d8
JonasRegener <regener.jonas@gmail.com>
2022-05-29 20:46:38 +0200

    responsive design for portfolio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+
+    apiKey: "AIzaSyB2r5GmtSHxT46fxlgevvXFVeoc-mu36X4",
+
+    authDomain: "ring-of-fire-39352.firebaseapp.com",
+
+    projectId: "ring-of-fire-39352",
+
+    storageBucket: "ring-of-fire-39352.appspot.com",
+
+    messagingSenderId: "991649012350",
+
+    appId: "1:991649012350:web:1b8897beb65d4b22a7562f",
+
+    measurementId: "G-MP416NS2EB"
+
+  }
+
 };
